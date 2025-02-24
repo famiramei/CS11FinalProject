c319bad7e98e75ffbfb8939a051d28db46bdd4d0
primcaballeda <p.caballeda.539159@umindanao.edu.ph>
2025-02-24 16:09:49 +0800

    Display subjects completed

--- project.asm
+++ project.asm
@@ -5,7 +5,7 @@ option casemap :none
 include \masm32\include\windows.inc 
 include \masm32\include\kernel32.inc
 include \masm32\include\masm32.inc 
-
+ 
 includelib \masm32\lib\kernel32.lib
 includelib \masm32\lib\masm32.lib
 
